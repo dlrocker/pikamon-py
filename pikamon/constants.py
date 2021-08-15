efca97d1d57db46134a51fb7bc54ac0e3d2c280d
David Rocker <david.rocker1994@gmail.com>
2021-08-15 13:32:33 -0400

    Add logic to spawn a pokemon and keep track of pokemon in cache (#4)
    * Add logic to spawn a pokemon and keep track of pokemon in cache
    
    For non-bot command messages and non-bot messages, implemented logic to spawn a pokemon based on a spawn probability. If a pokemon is spawned, it is maintained in the cache until it expires.
    
    * pydoc

--- pikamon/constants.py
+++ pikamon/constants.py
@@ -1,2 +1,11 @@
 
 COMMAND_PREFIX = "p!ka "
+
+# Probability that a pokemon is Spawned if there is no pokemon currently in the channel. Should be between 0 and 1
+SPAWN_RATE = .5
+
+
+class Cache:
+    """Constants class for Time To Live (TTL) Cache"""
+    MAX_SIZE = 258
+    TTL = 5
