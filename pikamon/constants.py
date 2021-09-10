d53b07d7ba473a0f3c5cde1479f16e87dfadfd83
David Rocker <david.rocker1994@gmail.com>
2021-09-09 21:05:49 -0400

    Pass mutable cache to commands. Persist pokemon that are caught (#24)

--- pikamon/constants.py
+++ pikamon/constants.py
@@ -1,17 +1,26 @@
 # Prefix used for the Bot commands
-COMMAND_PREFIX = "p!ka "
+COMMAND_PREFIX = "p!ka"
 
 # SQLite database name
 DATABASE_CONFIG_PATH_ENV_VAR = "DATABASE_CONFIG_PATH"
 DATABASE_NAME = "pokemon.db"
 
+# SQLite database tables
+USER_TABLE = "users"
+POKEMON_TABLE = "pokemon"
+
 # Probability that a pokemon is Spawned if there is no pokemon currently in the channel. Should be between 0 and 1
 SPAWN_RATE = 1
-# Maximum allowed pokemon ID - it is the max ID number the pokemon API supports
+
+# Maximum allowed pokemon ID that the pokemon API supports
 MAX_POKEMON_ID = 807
+
+# Minimum/Maximum pokemon level
+MIN_POKEMON_LEVEL = 1
+MAX_POKEMON_LEVEL = 100
 
 
 class Cache:
     """Constants class for Time To Live (TTL) Cache"""
-    MAX_SIZE = 258
-    TTL = 5
+    MAX_SIZE = 258  # Max number of channels supported
+    TTL = 20        # Max lifetime of channel in cache - in seconds
